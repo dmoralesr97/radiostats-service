c515d0c33ac9430667635b9f14d9ea4cea66d32c
David Morales <davidmorales.thoughtworks@latam.com>
2026-07-04 13:07:39 -0500

    Initial commit from Specify template

---
+++ src/main/java/com/morales/radiostatsservice/RadiostatsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.morales.radiostatsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiostatsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadiostatsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/morales/radiostatsservice/RadiostatsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morales.radiostatsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiostatsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
